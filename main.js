e23795c5120d8a2986b87e9f6ee339edd7d2d10d
Melyssa Moya <moyamelyssa@gmail.com>
2023-10-11 16:46:10 +0000

    feat(main.js): Acrescentado variáveis para armazenar o nome dos candidatos e mudanças na exibição do alert para exibir o nome dos candidatos

--- main.js
+++ main.js
@@ -44,21 +44,32 @@
 // }
 
 function urnaEletronica(){
-    let candidato1 = 0;
-    let candidato2 = 0;
-    let candidato3 = 0;
-    let votoBranco = 0;
-    let votoNulo = 0;
-    let ganhador = "";
+    let 
+    candidato1 = "",
+    candidato2 = "",
+    candidato3 = "",
+    ganhador = "";
+
+    let 
+    votoC1 = 0,
+    votoC2 = 0,
+    votoC3 = 0,
+    votoBranco = 0,
+    votoGanhador = 0,
+    votoNulo = 0;
+
+    candidato1 = prompt("Digite o nome do candidato n° 1: ");
+    candidato2 = prompt("Digite o nome do candidato n° 2: ");
+    candidato3 = prompt("Digite o nome do candidato n° 3: ");
 
     do{
-        operacao = parseInt(prompt("Digite o número correspondente ao seu voto:\n\n[1] Candidato 1 \n[2] Candidato 2\n[3] Candidato 3\n[5] Branco\n[8] Nulo\n[0] Encerrar operação\n"));
+        operacao = parseInt(prompt(`Digite o número correspondente ao seu voto:\n\n[1] ${candidato1}\n[2] ${candidato2}\n[3] ${candidato3}\n[5] Branco\n[8] Nulo\n[0] Encerrar operação\n`));
         if(operacao === 1){
-            candidato1 += 1;
+            votoC1 += 1;
         } else if(operacao === 2){
-            candidato2 += 2;
+            votoC2 += 1;
         } else if(operacao === 3){
-            candidato3 += 3;
+            votoC3 += 1;
         } else if(operacao === 5){
             votoBranco += 1;
         } else if(operacao === 8){
@@ -70,15 +81,20 @@ function urnaEletronica(){
         }
     } while(operacao !== 0);
 
-    if(candidato1 > candidato2 && candidato1 > candidato3){
-        ganhador = "Candidato 1";
-    } else if(candidato2 > candidato1 && candidato2 > candidato3){
-        ganhador = "Candidato 2";
-    } else if(candidato3 > candidato1 && candidato3 > candidato2){
-        ganhador = "Candidato 3";
+    if(votoC1 > votoC2 && votoC1 > votoC3){
+        ganhador = candidato1;
+        votoGanhador = votoC1;
+    } else if(votoC2 > votoC1 && votoC2 > votoC3){
+        ganhador = candidato2;
+        votoGanhador = votoC2;
+    } else if(votoC3 > votoC1 && votoC3 > votoC2){
+        ganhador = candidato3;
+        votoGanhador = votoC3;
     } else {
-        alert("Empate! Sem ganhadores.")
+        alert(`Empate! Sem ganhadores.\nA quantidade de votos brancos e nulos foram de: ${votoBranco} e ${votoNulo}`);
     }
-
-    alert(`O ganhador foi: ${ganhador}.\nA quantidade de votos brancos e nulos foram de: ${votoBranco} ${votoNulo}.`);
+    
+    if(ganhador !== ""){
+        alert(`O ganhador foi: ${ganhador} com ${votoGanhador} votos.\nA quantidade de votos brancos e nulos foram de: ${votoBranco} e ${votoNulo}.`);
+    }
 }
