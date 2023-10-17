2c60ebb6ab6c17f9498e1ddd9ef1975b9c175bd8
Melyssa Moya <moyamelyssa@gmail.com>
2023-10-17 18:52:43 +0000

    feat(main.js): Foi acrescentada novas funcionalidades para a validação do fim da operação e porcentagem de votos)

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-// Estrutura switch-case
-
-// const opcao = 3;
-
-// switch(opcao) {
- //    case 1:
- //        console.log("A opção é 1");
- //        break;
- //    case 2:
- //        console.log("A opção é 2");
- //        break;
- //    default:
- //        console.log("Não é nenhuma das opções.")
-// }
-
-// Equivalente a estrutura switch-case
-//if(opcao == 1) {//
-//  console.log("A opção é 1");
-//} else if(opcao == 2) {//
-//    console.log("A opção é 2");
-//}else {
-//    console.log("Não é nenhuma das opções.")
-//}
-
-// Estrutura do-while 
-
-  // let contador = 1;
-
-// do {
-//     console.log("Instrução");
-//     contador += 1
-// } while (contador <= 10);
-
-// Estrutura while
-
-// while (opcao !== 0) {
-//     console.log("Instrução 1");
-//     console.log("Instrução 2");
-//     console.log("Instrução 3");
-//     console.log("Instrução 4");
-//     console.log("Instrução 5");
-//     console.log("Instrução 6");
-//     console.log("Instrução 7");
-// }
 
 function urnaEletronica(){
     let 
     candidato1 = "",
     candidato2 = "",
     candidato3 = "",
+    fim = "",
     ganhador = "";
 
     let 
@@ -56,14 +13,17 @@ function urnaEletronica(){
     votoC3 = 0,
     votoBranco = 0,
     votoGanhador = 0,
+    porcentagem = 0,
     votoNulo = 0;
+
+    let condicao = true
 
     candidato1 = prompt("Digite o nome do candidato n° 1: ");
     candidato2 = prompt("Digite o nome do candidato n° 2: ");
     candidato3 = prompt("Digite o nome do candidato n° 3: ");
 
-    do{
-        operacao = parseInt(prompt(`Digite o número correspondente ao seu voto:\n\n[1] ${candidato1}\n[2] ${candidato2}\n[3] ${candidato3}\n[5] Branco\n[8] Nulo\n[0] Encerrar operação\n`));
+    do {
+        operacao = parseInt(prompt(`Digite o número correspondente ao seu voto:\n\n[1] ${candidato1}\n[2] ${candidato2}\n[3] ${candidato3}\n[5] Branco\n[8] Nulo`));
         if(operacao === 1){
             votoC1 += 1;
             alert(`Candidato ${candidato1} votado com sucesso!`)
@@ -79,27 +39,34 @@ function urnaEletronica(){
         } else if(operacao === 8){
             votoNulo += 1;
             alert('Voto nulo registrado com sucesso!')
-        } else if(operacao === 0) {
-            alert("Encerrando a operação");
+        } else if(operacao === 150714) {
+            fim = prompt("Você tem certeza que deseja encerrar a operação? [S/N]")
+                if (fim === "S" || fim === "s") {
+                    alert("Encerrando a operação!");
+                    condicao = false;
+                }
         } else {
             alert("Operação inválida");
         }
-    } while(operacao !== 0);
+    } while(condicao);
 
     if(votoC1 > votoC2 && votoC1 > votoC3){
         ganhador = candidato1;
-        votoGanhador = votoC1;
+        votoGanhador = votoC1 + votoBranco;
     } else if(votoC2 > votoC1 && votoC2 > votoC3){
         ganhador = candidato2;
-        votoGanhador = votoC2;
+        votoGanhador = votoC2 + votoBranco;
     } else if(votoC3 > votoC1 && votoC3 > votoC2){
         ganhador = candidato3;
-        votoGanhador = votoC3;
+        votoGanhador = votoC3 + votoBranco;
     } else {
         alert(`Empate! Sem ganhadores.\nA quantidade de votos brancos e nulos foram de: ${votoBranco} e ${votoNulo}`);
     }
-    
+
+    porcentagem = (votoGanhador / (votoC1 + votoC2 + votoC3 + votoNulo)) * 100;
+
     if(ganhador !== ""){
-        alert(`O ganhador foi: ${ganhador} com ${votoGanhador} votos.\nA quantidade de votos brancos e nulos foram de: ${votoBranco} e ${votoNulo}.`);
+        alert(`O ganhador foi: ${ganhador} com ${porcentagem.toFixed(2)}% dos votos.\nA quantidade de votos brancos e nulos foram de: ${votoBranco} e ${votoNulo}.`);
     }
+    
 }
